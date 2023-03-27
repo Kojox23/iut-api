a80b2692d70ac1b39088a8f0534c000d536d245d
Kojox23 <demaille.lucas@gmail.com>
2023-03-27 14:07:58 +0200

    modif

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-const cors = require("cors");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var studentRouter = require("./routes/student");
