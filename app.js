b742ef86debd54a1e89afd3e1d817cf107ff1f45
Kojox23 <demaille.lucas@gmail.com>
2023-03-27 13:29:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var studentRouter = require('./routes/student');
+
+var app = express();
+// Intégration de la bdd
+var connectionString = "mongodb+srv://Darunia:100902@darunia.mf9cjcm.mongodb.net/test";
+var mongoDB = process.env.MONGODB_URI || connectionString;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/student', studentRouter);
+
+module.exports = app;
